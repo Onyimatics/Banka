8242202b469053e21b7ad67c74354389d010207e
Onyinye <ezikeonyinyefavour@gmail.com>
2019-04-04 13:15:12 +0100

    chore(testcovereage):create travisci-coveralls-test coverage -create a workable test coverage [Delivers #165119567]

---
+++ server/model/users.js
@@ -0,0 +1,97 @@
+const users = [
+    {
+    id : 1 ,
+    email : 'ezikeonyinyefavour@gmail.com' ,
+    firstName : 'Onyinye' ,
+    lastName : 'Ezike' ,
+    password : '123456789' ,
+    type : 'staff' , // client or staff
+    isAdmin : true , // must be a staff user account
+    },
+    {
+        id : 2 ,
+        email : 'mercyjohnson@gmail.com' ,
+        firstName : 'Mercy' ,
+        lastName : 'Johnson' ,
+        password : '123456789' ,
+        type : 'staff' , // client or staff
+        isAdmin : false , // must be a staff user account
+        },
+        {
+            id : 3 ,
+            email : 'jackieappiah@gmail.com' ,
+            firstName : 'Jackie' ,
+            lastName : 'Appiah' ,
+            password : '123456789' ,
+            type : 'staff' , // client or staff
+            isAdmin : false , // must be a staff user account
+            },
+            {
+                id : 4 ,
+                email : 'johndumelo@gmail.com' ,
+                firstName : 'John' ,
+                lastName : 'Dumelo' ,
+                password : '123456789' ,
+                type : 'staff' , // client or staff
+                isAdmin : false , // must be a staff user account
+                },
+                {
+                    id : 5 ,
+                    email : 'desmondelliot@gmail.com' ,
+                    firstName : 'Desmond' ,
+                    lastName : 'Elliot' ,
+                    password : '123456789' ,
+                    type : 'staff' , // client or staff
+                    isAdmin : false , // must be a staff user account
+                    },
+                    {
+                        id : 6 ,
+                        email : 'jeniferakindele@gmail.com' ,
+                        firstName : 'Jenifer' ,
+                        lastName : 'Akindele' ,
+                        password : '123456789' ,
+                        type : 'client' , // client or staff
+                        isAdmin : false , // must be a staff user account
+                        },
+                        {
+                            id : 7 ,
+                            email : 'emekaike@gmail.com' ,
+                            firstName : 'Emeka' ,
+                            lastName : 'Ike' ,
+                            password : '123456789' ,
+                            type : 'client' , // client or staff
+                            isAdmin : false , // must be a staff user account
+                            },
+                            {
+                                id : 8 ,
+                                email : 'markzuckerberg@gmail.com' ,
+                                firstName : 'Mark' ,
+                                lastName : 'Zuckerberg' ,
+                                password : '123456789' ,
+                                type : 'client' , // client or staff
+                                isAdmin : true , // must be a staff user account
+                                },
+                                {
+                                    id : 9 ,
+                                    email : 'alikodangote@gmail.com' ,
+                                    firstName : 'Aliko' ,
+                                    lastName : 'Dangote' ,
+                                    password : '123456789' ,
+                                    type : 'client' , // client or staff
+                                    isAdmin : false , // must be a staff user account
+                                    },
+                                    {
+                                        id : 10 ,
+                                        email : 'femiotedola@gmail.com' ,
+                                        firstName : 'Femi' ,
+                                        lastName : 'Otedola' ,
+                                        password : '123456789' ,
+                                        type : 'client' , // client or staff
+                                        isAdmin : false , // must be a staff user account
+                                        },
+                                        
+]
+
+
+
+export default users;
