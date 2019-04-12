53b7f3f2aad88e8aaff52018550b9fe7942d6cfd
Onyinye Ezike <ezikeonyinyefavour@gmail.com>
2019-04-12 20:45:08 +0100

    feat(accounts):consume admin/staff view a specific account -consume admin/staff can view a specific account [Delivers #165310434]

--- server/spec/account.test.js
+++ server/spec/account.test.js
@@ -23,21 +23,21 @@ describe('All Test Report for Banka App', () => {
           done();
         });
     });
-    // it('should fetch an account', (done) => {
-    //   chai.request(app)
-    //     .get(`${accountUrl}/9901234567`)
-    //     .set('authorization', staff1)
-    //     .end((err, res) => {
-    //       expect(res.body.status).to.equal(200);
-    //       expect(res.body.message).to.equal('Account details Successfully Retrieved');
-    //       expect(res).to.have.status(200);
-    //       expect(res.body).to.be.an('object');
-    //       // eslint-disable-next-line no-unused-expressions
-    //       expect(res).to.be.json;
-    //       expect(res.body.data.accountNumber).to.equal(9901234567);
-    //       done();
-    //     });
-    // });
+    it('should fetch an account', (done) => {
+      chai.request(app)
+        .get(`${accountUrl}/9901234567`)
+        .set('authorization', staff1)
+        .end((err, res) => {
+          expect(res.body.status).to.equal(200);
+          expect(res.body.message).to.equal('Account details Successfully Retrieved');
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('object');
+          // eslint-disable-next-line no-unused-expressions
+          expect(res).to.be.json;
+          expect(res.body.data.accountNumber).to.equal(9901234567);
+          done();
+        });
+    });
     it('should successfully create an account ', (done) => {
       chai.request(app)
         .post(accountUrl)
@@ -53,34 +53,34 @@ describe('All Test Report for Banka App', () => {
           done();
         });
     });
-    // it('should fail for invalid user account', (done) => {
-    //   chai.request(app)
-    //     .post(accountUrl)
-    //     .set('authorization', admin1)
-    //     .send({
-    //       type: 'savings',
-    //       id: 22,
-    //     })
-    //     .end((err, res) => {
-    //       expect(res.body).to.be.an('object');
-    //       expect(res.body.status).to.equal(404);
-    //       expect(res.body.message).to.equal('User account not found');
-    //       done();
-    //     });
-    // });
-    // it('should not create account if a user does not choose account type', (done) => {
-    //   chai.request(app)
-    //     .post(accountUrl)
-    //     .set('authorization', 'invalidtoken')
-    //     .send({
-    //       type: 'savings',
-    //       id: 7,
-    //     })
-    //     .end((err, res) => {
-    //       expect(res.body.status).to.equal(401);
-    //       expect(res.body.message).to.equal('You need to login.');
-    //       done();
-    //     });
-    // });
+    it('should fail for invalid user account', (done) => {
+      chai.request(app)
+        .post(accountUrl)
+        .set('authorization', admin1)
+        .send({
+          type: 'savings',
+          id: 22,
+        })
+        .end((err, res) => {
+          expect(res.body).to.be.an('object');
+          expect(res.body.status).to.equal(404);
+          expect(res.body.message).to.equal('User account not found');
+          done();
+        });
+    });
+    it('should not create account if a user does not choose account type', (done) => {
+      chai.request(app)
+        .post(accountUrl)
+        .set('authorization', 'invalidtoken')
+        .send({
+          type: 'savings',
+          id: 7,
+        })
+        .end((err, res) => {
+          expect(res.body.status).to.equal(401);
+          expect(res.body.message).to.equal('You need to login.');
+          done();
+        });
+    });
   });
 });
