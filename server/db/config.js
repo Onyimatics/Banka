9baa7e254b1f4b30f11bd79710966883cfa61521
Onyinye Ezike <ezikeonyinyefavour@gmail.com>
2019-04-21 18:45:26 +0100

    ft(signup): convert signup endpoint for database -convert signup endpoint for database [Finishes #165485803]

--- server/db/config.js
+++ server/db/config.js
@@ -3,9 +3,7 @@ import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const connetionstring = process.env.DB_TEST_URL;
-
-// const connetionstring = process.env.DB_CONNECTIONSTRING;
+const connetionstring = process.env.DATABASE_URL;
 
 const pool = new Pool({
   connectionString: connetionstring,
