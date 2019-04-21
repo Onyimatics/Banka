e8976edbb9dac319847f93ffb1035e2ff70de373
Onyinye Ezike <ezikeonyinyefavour@gmail.com>
2019-04-21 23:53:16 +0100

    ft(signin): convert signin endpoint to database -convert signin endpoint to database [Finishes #165485851]

--- server/helper/messages/validationMessages.js
+++ server/helper/messages/validationMessages.js
@@ -1,9 +1,9 @@
 class ValidationMessages {}
 
-ValidationMessages.email = 'Enter valid email, firstName & lastName.';
-ValidationMessages.userName = 'Enter valid email, firstName & lastName.';
+ValidationMessages.email = 'Enter a valid email.';
+ValidationMessages.userName = 'Enter a valid firstName & lastName.';
 ValidationMessages.password = 'Enter a password with at least 8 characters.';
-ValidationMessages.firstName = 'Enter valid email, firstName & lastName.';
-ValidationMessages.lastName = 'Enter valid email, firstName & lastName.';
+ValidationMessages.firstName = 'Enter a valid firstName.';
+ValidationMessages.lastName = 'Enter a valid lastName.';
 ValidationMessages.accountNumber = 'Enter a valid Account Number';
 export default ValidationMessages;
