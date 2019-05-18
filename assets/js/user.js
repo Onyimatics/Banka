bf59c1f81301f8ac0e832b883842bb63482c02fa
Onyinye Ezike <ezikeonyinyefavour@gmail.com>
2019-05-18 17:37:51 +0100

    ft(account details): add functionality to view specific account details
    - add functionality to admin can view specific account details usinf fetch API
    - add functionality to admin can view the transactions under a specific account details using fetch API
    
    [Finishes #166104176]

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-return-assign */
 /* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
 const userDetails = JSON.parse(localStorage.getItem('userDetails'));
 const token = localStorage.getItem('token');
 
